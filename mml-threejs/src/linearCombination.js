51c593852ad99c90dfc0868eba0f3b6adecfb0bb
michael Jo <csz0209@gmail.com>
2026-07-28 13:20:07 +0100

    🚧temp: check whether module task works or not

---
+++ mml-threejs/src/linearCombination.js
@@ -0,0 +1,13 @@
+import * as THREE from "three";
+
+export function linearCombination(c3, c2, c3) {;
+  // standard basis vector
+  const e1 = new THREE.Vector3(1, 0, 0);
+  const e2 = new THREE.Vector3(0, 1, 0);
+  const e3 = new THREE.Vector3(0, 0, 1);
+
+    return new THREE.Vector3()
+    .add(e1.clone().multiplyScalar(c1))
+    .add(e2.clone().multiplyScalar(c2))
+    .add(e3.clone().multiplyScalar(c3));
+}
